8dcb5804731f54ebf9a237d76551e6fccdce9ad2
Leonardo Medeiros <leo-sbs@live.com>
2024-01-17 21:32:44 -0300

    começo da api

---
+++ src/main/java/med/voll/api/controller/HelloController.java
@@ -0,0 +1,17 @@
+package med.voll.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String helloWorld (){
+
+        return "Hello World Spring !";
+    }
+
+}
